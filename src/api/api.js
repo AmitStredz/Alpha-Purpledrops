7075e197943bc0bda43dd09c09fff18ad61c1ce0
AmitStredz <ponnan2112@gmail.com>
2025-06-19 20:34:50 +0530

    api endpoint updated to 'ourbot'

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
 export const URL =
-  "https://dca-bot-alpha-325e210a41d8.herokuapp.com/api/users/";
+  "https://app.ourbot.in/api/users/";
 
 export const LOGIN_URL = URL + "login/";
 export const SIGN_UP_URL = URL + "signup/";
 export const PLAN_URL = URL + "select-plan/";
 
-export const BASE_URL = "https://dca-alpha-bot-be-do.alpharoboticsllp.com";
+export const BASE_URL = "https://app.ourbot.in";
